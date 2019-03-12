37f72664d9a4e98691f50c01f32ccecc0bd40305
Cosmin <mirceacosmin@gmail.com>
2019-03-12 15:51:12 +0000

    #11 Saving entry station completed

--- oystercard/lib/oystercard.rb
+++ oystercard/lib/oystercard.rb
@@ -2,26 +2,34 @@ class Oystercard
   START_BALANCE = 0
   MAX_BALANCE = 90
   MINIMUM_FARE = 1
-  attr_reader :balance
+  attr_reader :balance, :entry_station
 
   def initialize(balance = START_BALANCE, journey = false)
     @balance = balance
     @journey = journey
   end
 
+  ### attr reader creates this
+  # def test
+  #   @test
+  # end
+  ###
+
   def top_up(amount)
     fail 'Max balance reached' if (amount + balance) > MAX_BALANCE
     @balance += amount
   end
 
-  def touch_in
+  def touch_in(station)
     fail "Not enough funds - Minimum balance needed is: #{MINIMUM_FARE}" if @balance < MINIMUM_FARE
+    @entry_station = station
     @journey = true
   end
 
   def touch_out
    deduct(MINIMUM_FARE)
-    @journey = false
+   @entry_station = nil
+   @journey = false
   end
 
   def in_journey?
--- oystercard/spec/oystercard_spec.rb
+++ oystercard/spec/oystercard_spec.rb
@@ -2,6 +2,7 @@ require 'oystercard'
 
 describe Oystercard do
   subject(:card) { Oystercard.new }
+  let(:station) {double}
 
   describe '#Balance' do
     it 'default balance is zero' do
@@ -27,14 +28,14 @@ describe Oystercard do
   context '#deduct' do
 
     it "it doesn't allow entry - balance below Minimum fare" do
-      expect { card.touch_in }.to raise_error "Not enough funds - Minimum balance needed is: #{Oystercard::MINIMUM_FARE}"
+      expect { card.touch_in(station) }.to raise_error "Not enough funds - Minimum balance needed is: #{Oystercard::MINIMUM_FARE}"
     end
   end
 
   describe '#journey' do
 
     it 'allows the customer to touch in and pass through the barriers' do
-      expect(card).to respond_to(:touch_in)
+      expect(card).to respond_to(:touch_in).with(1)
     end
 
     it 'allows the customer to touch out and pass through the barriers' do
@@ -43,7 +44,7 @@ describe Oystercard do
 
     it 'starts a journey when you touch in' do
       card.top_up(Oystercard::MINIMUM_FARE)
-      expect(card.touch_in).to eq(true)
+      expect(card.touch_in(station)).to eq(true)
     end
 
     it 'ends a journey when you touch out' do
@@ -53,7 +54,7 @@ describe Oystercard do
     it 'is in journey' do
       card.top_up(Oystercard::MINIMUM_FARE)
       # when you create the card in subject(card), journey is set to false 
-      card.touch_in # journey is set to true true
+      card.touch_in(station) # journey is set to true true
       # expect([]).to be_empty # this calls [].empty? to return true
       expect(card).to be_in_journey # this calls card.in_journey? to return true
 
@@ -69,6 +70,20 @@ describe Oystercard do
     it 'reduces the customers balance by the minimum fare' do
       expect { card.touch_out }.to change{ card.balance }.by -Oystercard::MINIMUM_FARE
     end
+
+    it 'saves station' do
+      card.top_up(Oystercard::MINIMUM_FARE)
+      card.touch_in(station)
+      expect(card.entry_station).to eq(station)
+    end
+
+    it 'forgets entry station on touch out' do
+      card.top_up(1)
+      card.touch_in(station)
+      card.touch_out
+      expect(card.entry_station).to be_nil
+    end
+
   end
 end
 
